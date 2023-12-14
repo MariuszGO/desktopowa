bd784529bf9a5b62d42c32a5973b856d2ec4eaa5
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-12-14 12:10:47 +0100

    14.12.2023

--- desktopowa/MainWindow.xaml.cs
+++ desktopowa/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace desktopowa
     /// </summary>
     public partial class MainWindow : Window
     {
+        string haslo = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,6 +30,76 @@ namespace desktopowa
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            haslo = "";
+            int ilosc = Convert.ToInt32(ile.Text);
+            int nr;
+            string znaki = "abcdefghijklmnoprstuwxyvzq";
+            Random los = new Random();
+            
+
+            if(wielkie.IsChecked == true)
+            {
+                string wielkie_znaki = "ABCDEFGHIJKLMNOPRSTWUXYZQ";
+
+
+                nr = los.Next(0, 25);
+
+                haslo += wielkie_znaki[nr];
+                ilosc--;
+            }
+
+            if (cyfra.IsChecked == true)
+            {
+                string cyfry = "0123456789";
+
+
+                nr = los.Next(0, 9);
+
+                haslo += cyfry[nr];
+                ilosc--;
+            }
+
+            //
+
+            if (znak.IsChecked == true)
+            {
+                string tablica_znakow = "!@#$%^&*()_+-=";
+
+
+                nr = los.Next(0, 13);
+
+                haslo += tablica_znakow[nr];
+                ilosc--;
+            }
+
+
+
+            for (int i = 0; i < ilosc; i++)
+            {
+               nr = los.Next(0,25);
+                haslo += znaki[nr];
+
+            }
+            
+            MessageBox.Show(haslo);
+
+            
+           // wynik.Text = haslo;
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string efekt_koncowy;
+
+            efekt_koncowy = "Dane pracownika: " + imie.Text + " " + nazwisko.Text
+                            + " " + stanowisko.Text + "Hasło: " + haslo;
+             
+            MessageBox.Show(efekt_koncowy);
         }
     }
 }
